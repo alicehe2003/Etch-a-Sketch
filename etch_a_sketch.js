3cb2e4dc5404e162059a0edffcb68dac9993139b
alicehe2003 <140996846+alicehe2003@users.noreply.github.com>
2024-05-16 18:17:41 -0700

    Generates random colour upon hover.

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,4 +1,4 @@
-// select square element 
+// select square elements
 const squares = document.querySelectorAll(".square"); 
 
 squares.forEach(square => {
@@ -6,5 +6,27 @@ squares.forEach(square => {
 })
 
 function changeColor(event) {
-    event.target.style.backgroundColor = "blue"; 
+    event.target.style.backgroundColor = getRandomColor(); 
+}
+
+// getRandomColor code from 
+// https://stackoverflow.com/a/1484514
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// reset the space 
+const resetButton = document.querySelector(".reset"); 
+
+resetButton.addEventListener("click", reset); 
+
+function reset() {
+    squares.forEach(square => {
+        square.style.backgroundColor = "aliceblue"
+    })
 }
